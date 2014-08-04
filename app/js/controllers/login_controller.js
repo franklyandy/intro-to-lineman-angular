81234fa310c29305790766d346526f4fd08798e1
Andrew Frank <afrank@contactusinc.com>
2014-08-04 09:50:40 -0400

    Adding e2e config.

--- app/js/controllers/login_controller.js
+++ app/js/controllers/login_controller.js
@@ -1,5 +1,6 @@
 angular.module("app").controller('LoginController', function($scope, $location, AuthenticationService) {
-  $scope.credentials = { username: "", password: "" };
+
+	$scope.credentials = { username: "", password: "" };
 
   var onLoginSuccess = function() {
     $location.path('/home');
